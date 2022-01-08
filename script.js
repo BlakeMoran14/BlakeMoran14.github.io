e7abeb4a7861c8610e602bd723e5e448b9573769
818523 <818523@mystma.org>
2022-01-08 20:55:10 +0000

    added portfolio

---
+++ script.js
@@ -0,0 +1,423 @@
+function _defineProperty(obj, key, value) {if (key in obj) {Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true });} else {obj[key] = value;}return obj;} // Made by Yago Estévez (Twitter: @yagoestevez.com)
+
+
+/***********************
+  Menu Component
+ ***********************/
+
+const Menu = props => {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: `menu-container ${props.showMenu}` }, /*#__PURE__*/
+    React.createElement("div", { className: "overlay" }), /*#__PURE__*/
+    React.createElement("div", { className: "menu-items" }, /*#__PURE__*/
+    React.createElement("ul", null, /*#__PURE__*/
+    React.createElement("li", null, /*#__PURE__*/
+    React.createElement("a", { href: "#welcome-section", onClick: props.toggleMenu }, "HOME")), /*#__PURE__*/
+
+
+
+    React.createElement("li", null, /*#__PURE__*/
+    React.createElement("a", { href: "#about", onClick: props.toggleMenu }, "ABOUT")), /*#__PURE__*/
+
+
+
+    React.createElement("li", null, /*#__PURE__*/
+    React.createElement("a", { href: "#projects", onClick: props.toggleMenu }, "PORTFOLIO")), /*#__PURE__*/
+
+
+
+    React.createElement("li", null, /*#__PURE__*/
+    React.createElement("a", { href: "#contact", onClick: props.toggleMenu }, "CONTACT"))), /*#__PURE__*/
+
+
+
+
+    React.createElement(SocialLinks, null))));
+
+
+
+};
+
+
+/***********************
+  Nav Component
+ ***********************/
+
+const Nav = props => {
+  return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement("nav", { id: "navbar" }, /*#__PURE__*/
+    React.createElement("div", { className: "nav-wrapper" }, /*#__PURE__*/
+    React.createElement("p", { className: "brand" }, "Blake", /*#__PURE__*/
+
+    React.createElement("strong", null, " Moran")), /*#__PURE__*/
+
+    React.createElement("a", {
+      onClick: props.toggleMenu,
+      className: props.showMenu === 'active' ? 'menu-button active' : 'menu-button' }, /*#__PURE__*/
+
+    React.createElement("span", null))))));
+
+
+
+
+
+};
+
+
+
+/***********************
+  Header Component
+ ***********************/
+
+const Header = props => {
+  return /*#__PURE__*/(
+    React.createElement("header", { id: "welcome-section" }, /*#__PURE__*/
+    React.createElement("div", { className: "forest" }), /*#__PURE__*/
+    React.createElement("div", { className: "silhouette" }), /*#__PURE__*/
+    React.createElement("div", { className: "moon" }), /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("h1", null, /*#__PURE__*/
+    React.createElement("span", { className: "line" }, "I do"), /*#__PURE__*/
+    React.createElement("span", { className: "line" }, "graphic design"), /*#__PURE__*/
+    React.createElement("span", { className: "line" }, /*#__PURE__*/
+    React.createElement("span", { className: "color" }, "&"), " code.")), /*#__PURE__*/
+
+
+    React.createElement("div", { className: "buttons" }, /*#__PURE__*/
+    React.createElement("a", { href: "#projects" }, "my portfolio"), /*#__PURE__*/
+    React.createElement("a", { href: "#contact", className: "cta" }, "get in touch")))));
+
+
+
+
+
+
+};
+
+
+/***********************
+  About Component
+ ***********************/
+
+const About = props => {
+  return /*#__PURE__*/(
+    React.createElement("section", { id: "about" }, /*#__PURE__*/
+    React.createElement("div", { className: "wrapper" }, /*#__PURE__*/
+    React.createElement("article", null, /*#__PURE__*/
+    React.createElement("div", { className: "title" }, /*#__PURE__*/
+    React.createElement("h3", null, "Who's this guy?"), /*#__PURE__*/
+    React.createElement("p", { className: "separator" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "desc full" }, /*#__PURE__*/
+    React.createElement("h4", { className: "subtitle" }, "My name is Blake."), /*#__PURE__*/
+    React.createElement("p", null, "I am a web developer and UX designer based in the beautiful state of Minnesota, USA"), /*#__PURE__*/
+
+
+
+    React.createElement("p", null, "I really enjoy solving problems as well as making things pretty and easy to use. I can't stop learning new things; the more, the better. I also love photography, a hobby I'm taking along since the good old film cameras.")), /*#__PURE__*/
+
+
+
+
+
+
+    React.createElement("div", { className: "title" }, /*#__PURE__*/
+    React.createElement("h3", null, "What does he do?"), /*#__PURE__*/
+    React.createElement("p", { className: "separator" })), /*#__PURE__*/
+
+    React.createElement("div", { className: "desc" }, /*#__PURE__*/
+    React.createElement("h4", { className: "subtitle" }, "I'm a programmer."), /*#__PURE__*/
+    React.createElement("p", null, "For the front-end I usually work with Javascript, either standalone or including popular frameworks like ReactJS and VueJS. I also make the web pretty by using Sass, CSS and, whenever needed, any of their friends: Bootstrap, Bulma, etc."), /*#__PURE__*/
+
+
+
+
+    React.createElement("p", null, "For the back-end I also work with Javascript (NodeJS, Express, MongoDB, etc). But, of course, whenever the project requires PHP, I do PHP as well (Wordpress, Laravel, etc).")), /*#__PURE__*/
+
+
+
+
+    React.createElement("div", { className: "desc" }, /*#__PURE__*/
+    React.createElement("h4", { className: "subtitle" }, "Also a designer."), /*#__PURE__*/
+    React.createElement("p", null, "I feel comfortable working with many Adobe products. Photoshop, Illustrator, InDesign, Lightroom or Xd are some kind of industry standards and I love working with them. I'm not limited to them, though: Gimp, Inkscape or Figma are also very valid applications that I've been working with."), /*#__PURE__*/
+
+
+
+
+
+    React.createElement("p", null, "User interfaces, brochures, books, branding... You name it! As I mentioned, creating pretty things is particularly important for me."))))));
+
+
+
+
+
+
+
+
+};
+
+
+/***********************
+  Project Component
+ ***********************/
+
+const Project = props => {
+  const tech = {
+    sass: 'fab fa-sass',
+    css: 'fab fa-css3-alt',
+    js: 'fab fa-js-square',
+    react: 'fab fa-react',
+    vue: 'fab fa-vuejs',
+    d3: 'far fa-chart-bar',
+    discordjs: 'fab fa-discord',
+    mongodb: 'fas fa-database',
+    node: 'fab fa-node' };
+
+
+  const link = props.link || 'http://';
+
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "project" }, /*#__PURE__*/
+    React.createElement("a", { className: "project-link", href: link, target: "_blank", rel: "noopener noreferrer" }, /*#__PURE__*/
+    React.createElement("img", { className: "project-image", src: props.img, alt: 'Screenshot of ' + props.title })), /*#__PURE__*/
+
+    React.createElement("div", { className: "project-details" }, /*#__PURE__*/
+    React.createElement("div", { className: "project-tile" }, /*#__PURE__*/
+    React.createElement("p", { className: "icons" },
+    props.tech.split(' ').map((t) => /*#__PURE__*/
+    React.createElement("i", { className: tech[t], key: t }))),
+
+
+    props.title, ' '),
+
+    props.children, /*#__PURE__*/
+    React.createElement("div", { className: "buttons" }, /*#__PURE__*/
+
+    React.createElement("a", { href: link, target: "_blank", rel: "noopener noreferrer" }, "View Project ", /*#__PURE__*/
+    React.createElement("i", { className: "fas fa-external-link-alt" }))))));
+
+
+
+
+
+};
+
+
+
+/***********************
+  Projects Component
+ ***********************/
+
+const Projects = props => {
+  return /*#__PURE__*/(
+    React.createElement("section", { id: "projects" }, /*#__PURE__*/
+    React.createElement("div", { className: "projects-container" }, /*#__PURE__*/
+    React.createElement("div", { className: "heading" }, /*#__PURE__*/
+    React.createElement("h3", { className: "title" }, "My Projects"), /*#__PURE__*/
+    React.createElement("p", { className: "separator" }), /*#__PURE__*/
+    React.createElement("p", { className: "subtitle" }, "Here's a list of ", /*#__PURE__*/
+    React.createElement("u", null, "most"), " of the projects I've been working on lately. All of these were build for",
+    ' ', /*#__PURE__*/
+    React.createElement("a", { href: "https://vo1ddev.tk", target: "_blank", rel: "noopener noreferrer" }, "Vo1dDev"), ", My personal development and management company. Go look at future projects there!")), /*#__PURE__*/
+
+
+
+
+
+
+    React.createElement("div", { className: "projects-wrapper" }, /*#__PURE__*/
+    React.createElement(Project, {
+      title: "GrapeBot.",
+      img: 'https://images.discordapp.net/avatars/825284353918763058/e84959a708416a0e67a2f4a772aaba8f.png?size=256',
+      tech: "js node discordjs",
+      link: "https://grape.vo1ddev.tk",
+      repo: "https://github.com/" }, /*#__PURE__*/
+
+    React.createElement("small", null, "Built using Node, Express, MongoDB, DisTube and DiscordJS."), /*#__PURE__*/
+
+
+    React.createElement("p", null, "This is a Discord Music Bot that allows users to control their servers with a simple and easy-to-use dashboard."))
+    
+    
+    ))));
+
+
+
+
+
+
+
+};
+
+
+
+/***********************
+  Contact Component
+ ***********************/
+
+const Contact = props => {
+  return /*#__PURE__*/(
+    React.createElement("section", { id: "contact" }, /*#__PURE__*/
+    React.createElement("div", { className: "container" }, /*#__PURE__*/
+    React.createElement("div", { className: "heading-wrapper" }, /*#__PURE__*/
+    React.createElement("div", { className: "heading" }, /*#__PURE__*/
+    React.createElement("p", { className: "title" }, "Want to ", /*#__PURE__*/
+    React.createElement("br", null), "contact me?"), /*#__PURE__*/
+
+
+    React.createElement("p", { className: "separator" }), /*#__PURE__*/
+    React.createElement("p", { className: "subtitle" }, "Please, use the form below or send an email to ",
+    '', /*#__PURE__*/
+    React.createElement("span", { className: "mail" }, "blake", /*#__PURE__*/
+
+    React.createElement("i", { className: "fas fa-at at" }), "vo1ddev", /*#__PURE__*/
+
+    React.createElement("i", { className: "fas fa-circle dot" }), "tk"), ":")), /*#__PURE__*/
+
+
+
+
+
+    React.createElement(SocialLinks, null)), /*#__PURE__*/
+
+    React.createElement("form", { id: "contact-form", action: "#" }, /*#__PURE__*/
+    React.createElement("input", { placeholder: "Name", name: "name", type: "text", required: true }), /*#__PURE__*/
+    React.createElement("input", { placeholder: "Email", name: "email", type: "email", required: true }), /*#__PURE__*/
+    React.createElement("textarea", { placeholder: "Message", type: "text", name: "message" }), /*#__PURE__*/
+    React.createElement("input", { className: "button", id: "submit", value: "Submit", type: "submit" })))));
+
+
+
+
+};
+
+
+
+/***********************
+  Footer Component
+ ***********************/
+
+const Footer = props => {
+  return /*#__PURE__*/(
+    React.createElement("footer", null, /*#__PURE__*/
+    React.createElement("div", { className: "wrapper" }, /*#__PURE__*/
+    React.createElement("h3", null, "THANKS FOR VISITING"), /*#__PURE__*/
+    React.createElement("p", null, "\xA9 ", new Date().getFullYear(), " Yago Est\xE9vez."), /*#__PURE__*/
+    React.createElement(SocialLinks, null))));
+
+
+
+};
+
+
+
+
+/***********************
+  Social Links Component
+ ***********************/
+
+const SocialLinks = props => {
+  return /*#__PURE__*/(
+    React.createElement("div", { className: "social" }, /*#__PURE__*/
+
+    React.createElement("a", {
+      id: "profile-link",
+      href: "https://github.com/blakemoran14",
+      target: "_blank",
+      rel: "noopener noreferrer",
+      title: "Link to author's GitHub Profile" },
+
+    ' ', /*#__PURE__*/
+    React.createElement("i", { className: "fab fa-github" })), /*#__PURE__*/
+
+    React.createElement("a", {
+      href: "https://blakemoran.carrd.co/",
+      target: "_blank",
+      rel: "noopener noreferrer",
+      title: "Link to author's carrd" },
+
+    ' ', /*#__PURE__*/
+    React.createElement("i", { className: "fas fa-id-card" }))));
+
+
+
+};
+
+
+
+/***********************
+  Main Component
+ ***********************/
+
+class App extends React.Component {constructor(...args) {super(...args);_defineProperty(this, "state",
+    {
+      menuState: false });_defineProperty(this, "toggleMenu",
+
+
+    () => {
+      this.setState(state => ({
+        menuState: !state.menuState ?
+        'active' :
+        state.menuState === 'deactive' ?
+        'active' :
+        'deactive' }));
+
+    });}
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement(React.Fragment, null, /*#__PURE__*/
+      React.createElement(Menu, { toggleMenu: this.toggleMenu, showMenu: this.state.menuState }), /*#__PURE__*/
+      React.createElement(Nav, { toggleMenu: this.toggleMenu, showMenu: this.state.menuState }), /*#__PURE__*/
+      React.createElement(Header, null), /*#__PURE__*/
+      React.createElement(About, null), /*#__PURE__*/
+      React.createElement(Projects, null), /*#__PURE__*/
+      React.createElement(Contact, null), /*#__PURE__*/
+      React.createElement(Footer, null)));
+
+
+  }
+
+  componentDidMount() {
+    const navbar = document.querySelector('#navbar');
+    const header = document.querySelector('#welcome-section');
+    const forest = document.querySelector('.forest');
+    const silhouette = document.querySelector('.silhouette');
+    let forestInitPos = -300;
+
+    window.onscroll = () => {
+      let scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+
+      if (scrollPos <= window.innerHeight) {
+        silhouette.style.bottom = `${parseInt(scrollPos / 6)}px`;
+        forest.style.bottom = `${parseInt(forestInitPos + scrollPos / 6)}px`;
+      }
+
+      if (scrollPos - 100 <= window.innerHeight)
+      header.style.visibility = header.style.visibility === 'hidden' && 'visible';else
+      header.style.visibility = 'hidden';
+
+      if (scrollPos + 100 >= window.innerHeight) navbar.classList.add('bg-active');else
+      navbar.classList.remove('bg-active');
+    };
+
+    (function navSmoothScrolling() {
+      const internalLinks = document.querySelectorAll('a[href^="#"]');
+      for (let i in internalLinks) {
+        if (internalLinks.hasOwnProperty(i)) {
+          internalLinks[i].addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector(internalLinks[i].hash).scrollIntoView({
+              block: 'start',
+              behavior: 'smooth' });
+
+          });
+        }
+      }
+    })();
+  }}
+
+
+
+ReactDOM.render( /*#__PURE__*/React.createElement(App, null), document.getElementById('app'));
